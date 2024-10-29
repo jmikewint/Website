22fbb5052f9b776e91d015b1ebf9e89857b64daf
Jayden Wint <jaymikecan1@gmail.com>
2024-10-29 14:10:22 -0400

    Everthing pushed

--- test.js
+++ test.js
@@ -23,3 +23,29 @@ function reOrder() {
     $('.left-child h2').text("fixed")
   }
 }
+
+
+// fade in grid items  ==================================
+
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var tags = $(".fadein")
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+
+    if ($(tag).offset().top < pageBottom) {
+      $(tag).addClass("visible")
+    } else {
+      $(tag).removeClass("visible")
+    }
+  }
+})
+
+
+
+
+setTimeout(() => {
+  console.log("Hello World");
+}, 3000);
